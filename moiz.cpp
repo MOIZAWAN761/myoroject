a42835bf1710629ddcd9b8ddce8f6c92ef4f54ec
MOIZAWAN761 <moizawan965@gmail.com>
2024-08-23 11:48:43 +0500

    Add files via upload

---
+++ moiz.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    class moiz
+    {
+    private:
+        /* data */
+    public:
+        moiz(/* args */);
+        ~moiz();
+    };
+    
+    moiz::moiz(/* args */)
+    {
+    }
+    
+    moiz::~moiz()
+    {
+    }
+    
+}
